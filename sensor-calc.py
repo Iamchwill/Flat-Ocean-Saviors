18bd322f79056023a29e56353a247377d50691fa
RavindraM23 <rmangar30@stuy.edu>
2022-04-05 17:01:41 -0400

    Python Sensor Code Modified

--- sensor-calc.py
+++ sensor-calc.py
@@ -7,6 +7,7 @@ import time
 import os
 import board
 import busio
+from statistics import mean
 
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor1 = adafruit_fxos8700.FXOS8700(i2c)
@@ -23,21 +24,21 @@ def pitch_am(accelX,accelY,accelZ):
 
 def yaw_am(accelX,accelY,accelZ,magX,magY,magZ):
     #TODO
-    mag_x = (magX * np.cos(pitch_am)) + ((magY * np.sin(roll_am) * np.sin(pitch_am)) + (magZ * np.cos(roll_am) * np.sin(pitch_am))
+    mag_x = (magX * np.cos(pitch_am)) + ((magY * np.sin(roll_am) * np.sin(pitch_am)) + (magZ * np.cos(roll_am) * np.sin(pitch_am)))
     return (180/np.pi)*np.arctan2(-mag_y, mag_x)
 
 #Activity 2: RPY based on gyroscope
 def roll_gy(prev_angle, delT, gyro):
-    #TODO
+    roll = prev_angle + (delT * gyro)
     return roll
 def pitch_gy(prev_angle, delT, gyro):
-    #TODO
+    pitch = prev_angle + (delT * gyro)
     return pitch
 def yaw_gy(prev_angle, delT, gyro):
-    #TODO
+    yaw = prev_angle + (delT * gyro)
     return yaw
 
-def set_initial(mag_offset = [0,0,0]):
+def set_initial(offset = [0,0,0]):
     #Sets the initial position for plotting and gyro calculations.
     print("Preparing to set initial angle. Please hold the IMU still.")
     time.sleep(3)
@@ -46,9 +47,9 @@ def set_initial(mag_offset = [0,0,0]):
     magX, magY, magZ = sensor1.magnetometer #gauss
     #Calibrate magnetometer readings. Defaults to zero until you
     #write the code
-    magX = magX - offset[0]
-    magY = magY - offset[1]
-    magZ = magZ - offset[2]
+    magX = magX + offset[0]
+    magY = magY + offset[1]
+    magZ = magZ + offset[2]
     roll = roll_am(accelX, accelY,accelZ)
     pitch = pitch_am(accelX,accelY,accelZ)
     yaw = yaw_am(accelX,accelY,accelZ,magX,magY,magZ)
@@ -56,16 +57,44 @@ def set_initial(mag_offset = [0,0,0]):
     return [roll,pitch,yaw]
 
 def calibrate_mag():
+    set_mag_X = []
+    set_mag_Y = []
+    set_mag_Z = []
+    set_accel_X = []
+    set_accel_Y = []
+    set_accel_Z = []
+    interval = 10
     #TODO: Set up lists, time, etc
-    #print("Preparing to calibrate magnetometer. Please wave around.")
-    #time.sleep(3)
-    #print("Calibrating...")
+    print("Preparing to calibrate magnetometer. Please wave around.")
+    time.sleep(2)
+    for i in range(interval):
+        mag_new_X, mag_new_Y, mag_new_Z = sensor1.magnetometer
+        accel_new_X, accel_new_Y, accel_new_Z = sensor1.accelerometer
+        set_mag_X.add(mag_new_X)
+        set_mag_Y.add(mag_new_Y)
+        set_mag_Z.add(mag_new_Z)
+        set_accel_X.add(accel_new_X)
+        set_accel_Y.add(accel_new_Y)
+        set_accel_Z.add(accel_new_Z)
+        time.sleep(1)
+    print("Calibrating...")
     #TODO: Calculate calibration constants
-   # print("Calibration complete.")
-    return [0,0,0]
+    magX = mean(set_mag_X)
+    magY = mean(set_mag_Y)
+    magZ = mean(set_mag_Z)
+    accelX = mean(set_accel_X)
+    accelY = mean(set_accel_Y)
+    accelZ = mean(set_accel_Z)
+    roll = roll_am(accelX, accelY,accelZ)
+    pitch = pitch_am(accelX,accelY,accelZ)
+    yaw = yaw_am(accelX,accelY,accelZ,magX,magY,magZ)    
+    print("Calibration complete.")
+    return [roll,pitch,yaw]
 
 def calibrate_gyro():
-    #TODO
+    set_theta_previous = []
+    set_delT = []
+    set_Wgyro = []
     #print("Preparing to calibrate gyroscope. Put down the board and do not touch it.")
     #time.sleep(3)
     #print("Calibrating...")
